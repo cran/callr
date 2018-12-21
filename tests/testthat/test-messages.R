8a2ebcdb7c6be7f1b349db1b3b6c581fef157929
Gábor Csárdi <csardi.gabor@gmail.com>
2018-12-21 08:40:07 +0000

    version 3.1.1

---
+++ tests/testthat/test-messages.R
@@ -0,0 +1,20 @@
+
+context("messages")
+
+test_that("messages in callr::r do not crash session", {
+  ret <- r(function() { cliapp::cli_text("fooobar"); 1 + 1 })
+  expect_identical(ret, 2)
+  gc()
+})
+
+test_that("messages in callr::r_bg do not crash session", {
+  rx <- r_bg(function() { cliapp::cli_text("fooobar"); 1 + 1 })
+  rx$wait(2000)
+  rx$kill()
+  expect_equal(rx$get_exit_status(), 0)
+
+  expect_equal(rx$get_result(), 2)
+  processx::processx_conn_close(rx$get_output_connection())
+  processx::processx_conn_close(rx$get_error_connection())
+  gc()
+})
